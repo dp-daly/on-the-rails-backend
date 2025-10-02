5ca48287a7e279a03d3e4a5d8620374458499b3a
Dan Daly <dan.daly@education.gov.uk>
2025-10-02 15:21:05 +0100

    Install RSpec, FactoryBot, and ShouldaMatchers and create model spec

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,2 +1,5 @@
 class Resource < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :link, presence: true
 end
---
+++ spec/models/resource_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Resource, type: :model do
+  describe 'validations' do
+    subject { build(:resource) }
+
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:link) }
+  end
+end
